684e03cab90cbc9659a0a1fcfd044d07829ef1d4
unknown <gnos2007@gmail.com>
2021-04-14 22:56:32 -0300

    Render basico, spritesheet começando ser implementada

--- src/graficos/Game.java
+++ src/graficos/Game.java
@@ -1,8 +1,14 @@
 package graficos;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable{ 
 	
@@ -10,14 +16,25 @@ public class Game extends Canvas implements Runnable{
 	private Thread thread;
 	//game rodando       v
 	private boolean isRunning = true;
-	private final int WIDTH = 160;
-	private final int HEIGHT = 120;
+	//Largura e Altura da tela ("constantes"):
+	private final int WIDTH = 1900;
+	private final int HEIGHT = 1080;
 	private final int SCALE = 3;
+	
+	private BufferedImage image;
+	
+	private Spritesheet sheet;
+	private BufferedImage player;
+	
 	
 	public Game()
 	{
+		sheet = new Spritesheet("/spritesheet.png");
+		player = sheet.getSprite(0, 0, 19, 22);
+		// ^ pega a sprite do player (usando o idoso como idle)
 		this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
 		initFrame();
+		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
 	}
 	
 	public void initFrame()
@@ -40,7 +57,15 @@ public class Game extends Canvas implements Runnable{
 	
 	public synchronized void stop()
 	{
+		isRunning = false;
 		
+		try 
+		{
+			thread.join();
+		} catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String args[]) 
@@ -51,13 +76,35 @@ public class Game extends Canvas implements Runnable{
 	
 	public void tick()
 	{
-		
+		x++;
 	}
 	
 	//renderizar os graficos
 	public void render()
 	{
+		//sequencia de Buffers na tela para otimizar a renderização
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null)
+		{
+			//visa performance:
+			this.createBufferStrategy(3);
+			return;
+		}
 		
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(23,255,36));
+		g.fillRect(0,0,WIDTH,HEIGHT);
+		g.setColor(Color.BLACK);
+		g.fillRect(20, 20, 80, 80);
+		//renderização do game:
+		
+		g.drawImage(player, x, 20, null);
+		//
+		g.dispose();
+		g = bs.getDrawGraphics(); 
+		//desenhar imagem
+		g.drawImage(image,0,0,WIDTH*SCALE,HEIGHT*SCALE,null);
+		bs.show();
 	}
 	
 	public void run()
@@ -97,6 +144,8 @@ public class Game extends Canvas implements Runnable{
 				timer += 1000;
 			}
 		}
+		
+		stop();
 		
 	}
 }
