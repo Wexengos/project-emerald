1e20769b96c185e2312e02604e24b8ae04d6ae17
unknown <gnos2007@gmail.com>
2021-04-15 13:46:43 -0300

    Comentarios adicionados no codigo

--- src/graficos/Game.java
+++ src/graficos/Game.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 import java.awt.image.BufferStrategy;
@@ -14,29 +15,39 @@ public class Game extends Canvas implements Runnable{
 	
 	public static JFrame frame;
 	private Thread thread;
+	
 	//game rodando       v
 	private boolean isRunning = true;
-	//Largura e Altura da tela ("constantes"):
+	
+	//Largura e Altura da tela ("constantes", não há a opção do jogador mudá-lo ainda):
 	private final int WIDTH = 1900;
 	private final int HEIGHT = 1080;
 	private final int SCALE = 3;
+	private int x;
 	
 	private BufferedImage image;
 	
 	private Spritesheet sheet;
-	private BufferedImage player;
+	private BufferedImage[] player;
+	private int frames = 0;
+	private int maxFrames = 20;
+	private int curAnimation = 0, maxAnimation = 2;
 	
-	
+	//Construtor, inicializa o game rodando a spritesheet e inicializando o player (QUE DEVE SER TRANSFORMADO EM CLASSE!!!)
 	public Game()
 	{
 		sheet = new Spritesheet("/spritesheet.png");
-		player = sheet.getSprite(0, 0, 19, 22);
-		// ^ pega a sprite do player (usando o idoso como idle)
+		player = new BufferedImage[2];
+		player[0] = sheet.getSprite(0, 0, 19, 22);
+		player[1] = sheet.getSprite(22,0, 19, 22);
+		
+		// ^ pega a sprite do player (animações de andar)
 		this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
 		initFrame();
 		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
 	}
 	
+	//inicializa a janela do game.
 	public void initFrame()
 	{
 		frame = new JFrame("Project Emerald");
@@ -48,13 +59,16 @@ public class Game extends Canvas implements Runnable{
 		frame.setVisible(true);
 	}
 	
+	//inicializar o game
 	public synchronized void start()
 	{
 		thread = new Thread(this);
 		isRunning = true;
+		//thread é para rodar mais de um processo ao mesmo tempo
 		thread.start();
 	}
 	
+	//paralizar o game
 	public synchronized void stop()
 	{
 		isRunning = false;
@@ -77,6 +91,16 @@ public class Game extends Canvas implements Runnable{
 	public void tick()
 	{
 		x++;
+		frames++;
+		if(frames > maxFrames)
+		{
+			frames = 0;
+			curAnimation++;
+			if(curAnimation >= maxAnimation)
+			{
+				curAnimation = 0;
+			}
+		}
 	}
 	
 	//renderizar os graficos
@@ -96,9 +120,10 @@ public class Game extends Canvas implements Runnable{
 		g.fillRect(0,0,WIDTH,HEIGHT);
 		g.setColor(Color.BLACK);
 		g.fillRect(20, 20, 80, 80);
-		//renderização do game:
-		
-		g.drawImage(player, x, 20, null);
+		//renderização do game: (em ordem, aquilo que vem numa linha abaixo está uma "camada" acima do que há acima dele
+		Graphics2D g2 = (Graphics2D) g;
+
+		g.drawImage(player[curAnimation], x, 90, null);
 		//
 		g.dispose();
 		g = bs.getDrawGraphics(); 
